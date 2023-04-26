045e41561ab67e9b5ba91f893ca33cb04ac217f0
loicDI <loic.dietmann@viacesi.fr>
2023-04-26 11:43:49 +0200

    Jour 2

--- dist/Database.php
+++ dist/Database.php
@@ -1,6 +1,6 @@
 <?php
 // Définition des paramètres de la connexion
-$servername = " http://172.16.47.156:32769/";; // nom du serveur de base de données
+$servername = "172.16.47.156";; // nom du serveur de base de données
 $username = "root"; // nom d'utilisateur
 $password = "toto"; // mot de passe
 $dbname = "transversegp2Jour2"; // nom de la base de données
